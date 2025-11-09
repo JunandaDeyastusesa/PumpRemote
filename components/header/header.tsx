638fc7ac772b45dfeedf064afa90615d6d360bbc
JunandaDeyastusesa <junandadeyastusesa@gmail.com>
2025-11-09 14:30:14 +0700

    HomePage, Power

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,26 +4,24 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { useNavigation } from "expo-router";
 
 type HeaderProps = {
-  title: string;
+    title: string;
 };
 
 const Header = ({ title }: HeaderProps) => {
     const navigation = useNavigation();
     return (
-        <SafeAreaView>
-            <Box bg="#F8FEFF" px="$4" py="$2">
-                <HStack justifyContent="space-between" alignItems="center">
-                    <View>
-                        <Heading size="md" mb="$0">{title}</Heading>
-                        <Text size="sm" mt="$0">Monitoring Pompa</Text>
-                    </View>
+        <Box bg="#F8FEFF" px="$4" py="$2">
+            <HStack justifyContent="space-between" alignItems="center">
+                <View>
+                    <Heading size="md" mb="$0">{title}</Heading>
+                    <Text size="sm" mt="$0">Monitoring Pompa</Text>
+                </View>
 
-                    <HStack>
-                        <Ionicons name="notifications-outline" size={24} color="black" />
-                    </HStack>
+                <HStack>
+                    <Ionicons name="notifications-outline" size={24} color="black" />
                 </HStack>
-            </Box>
-        </SafeAreaView>
+            </HStack>
+        </Box>
     )
 }
 
